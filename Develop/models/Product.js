9bcedbdc20f894bd94f2f4b784dab00f68a63eb1
Alex Delva <alexdelva16@gmail.com>
2023-11-08 16:33:50 -0500

    added changes to the product

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -15,6 +15,26 @@ Product.init(
       primaryKey: true,
       autoIncrement: true
     },
+  product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      },
+      defaultValue:10
+    },
   },
   {
     sequelize,
